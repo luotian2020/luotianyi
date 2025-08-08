26db206af40101e92e2ba7178d1debd9f2084abe
luotian2020 <2731741733@qq.com>
2025-08-08 23:39:22 +0800

    data3

--- data1.js
+++ data1.js
@@ -41,12 +41,53 @@ window.jsonData1 = {
       picture: "https://s3.bmp.ovh/imgs/2025/08/07/1d6d10b307edb05e.png"
     },
      {
-      name: "昆曲",
+      name: "杜丽娘",
       picture: "https://s3.bmp.ovh/imgs/2025/08/07/efa9c99cfa9ecd0e.png"
     },
      {
       name: "忆红莲",
       picture: "https://s3.bmp.ovh/imgs/2025/08/07/23ea5becc514dc71.png"
+    },
+
+    {
+      name: "三月雨",
+      picture: "https://s3.bmp.ovh/imgs/2025/08/08/00df11cb84e44e0b.webp"
+    },
+    {
+      name: "芒种",
+      picture: "https://s3.bmp.ovh/imgs/2025/08/08/d30eb0a000a7b4cd.webp"
+    },
+       {
+      name: "夜航星",
+      picture: "https://s3.bmp.ovh/imgs/2025/08/08/890c445feade134c.jpg"
+    },
+       {
+      name: "ACE-AI",
+      picture: "https://s3.bmp.ovh/imgs/2025/08/08/35d243826a83f754.webp"
+    },
+      {
+      name: "花辰月夕",
+      picture: "https://s3.bmp.ovh/imgs/2025/08/08/181db6b47537ecf0.jpg"
+    }  ,
+     {
+      name: "心律共鸣",
+      picture: "https://s3.bmp.ovh/imgs/2025/08/08/ce42c1e1e9517c67.jpg"
+    }  ,
+     {
+      name: "地下摇滚",
+      picture: "https://s3.bmp.ovh/imgs/2025/08/08/6eb47d9a6cff190b.png"
+    } ,
+     {
+      name: "金丝雀旗袍",
+      picture: "https://s3.bmp.ovh/imgs/2025/08/08/e7eb12d2df762601.png"
+    },
+     {
+      name: "无限共鸣",
+      picture: "https://s3.bmp.ovh/imgs/2025/08/08/b45d02996b1480bb.webp"
+    },
+     {
+      name: "权御天下",
+      picture: "https://s3.bmp.ovh/imgs/2025/08/08/c5691a5dcd3e3f87.jpg"
     }
 
   ]
